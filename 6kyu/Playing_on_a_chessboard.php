218cf725c72a980deda9e7a495bd1327aa2cce67
Andrius K <akvara@gmail.com>
2017-04-20 14:37:55 +0300

    Playing on a chessboard

---
+++ 6kyu/Playing_on_a_chessboard.php
@@ -0,0 +1,22 @@
+<?php
+include_once __DIR__.'/vendor/autoload.php';
+use PHPUnit\Framework\TestCase;
+
+function game(int $n): array {
+    $halves = $n ** 2;
+    return ($halves % 2 === 0) ? [intdiv($halves, 2)] : [$halves, 2];
+}
+
+
+class ChessBoardCases extends TestCase
+{
+    public function testBasics() {
+        $this->assertEquals([0], game(0));
+        $this->assertEquals([1, 2], game(1));
+        $this->assertEquals([32], game(8));
+        $this->assertEquals([800], game(40));
+    }
+}
+
+$a = new ChessBoardCases();
+$a->testBasics();
