1f21719f56e0c7b9b90a5758d2e243c8d71f4cfd
Andrius K <akvara@gmail.com>
2017-06-29 21:16:39 +0300

    [Py] Remove the minimum

--- KataTestSuite.py
+++ KataTestSuite.py
@@ -8,8 +8,16 @@ class Test:
             return
         print('\x1b[1;37;41m' + 'Not Ok' + '\x1b[0m')
 
-    def assert_equals(self, tobe, isit):
+    def it(self, description):
+        print description
+
+    def assert_equals(self, tobe, isit, whatiswrong = ''):
         if tobe == isit:
             print('\x1b[6;30;42m' + 'Ok' + '\x1b[0m')
             return
-        print('\x1b[1;37;41m' + 'No,' + '\x1b[0m' +  str(tobe) + '\x1b[1;37;41m' + " != " + '\x1b[0m'+ str(isit))
+        print('\x1b[1;37;41m' + 'No,' + '\x1b[0m'
+              +  str(tobe)
+              + '\x1b[1;37;41m' + " != " + '\x1b[0m'
+              + str(isit)
+              + whatiswrong
+              )
