be153e2d59044ee6a8fdcc067b77a277b45cedda
Andrius K <akvara@gmail.com>
2017-06-30 23:19:02 +0300

    [Py] Largest 5 digit number in a series

--- KataTestSuite.py
+++ KataTestSuite.py
@@ -2,11 +2,11 @@ class Test:
     def describe(self, description):
         self.doc = description
 
-    def expect(self, expr):
+    def expect(self, expr, whatiswrong = ''):
         if expr:
             print('\x1b[6;30;42m' + 'Ok' + '\x1b[0m')
             return
-        print('\x1b[1;37;41m' + 'Not Ok' + '\x1b[0m')
+        print('\x1b[1;37;41m' + 'Not Ok' + '\x1b[0m ' + whatiswrong)
 
     def it(self, description):
         print description
@@ -17,7 +17,7 @@ class Test:
             return
         print('\x1b[1;37;41m' + 'No,' + '\x1b[0m'
               +  str(tobe)
-              + '\x1b[1;37;41m' + " != " + '\x1b[0m'
-              + str(isit)
+              + '\x1b[1;37;41m' + " != " + '\x1b[0m '
+              + str(isit) + ' '
               + whatiswrong
               )
